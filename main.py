6d9c170ad58661ca4bb6f64a0a8e17aafb89b345
phatmai99 <maihungphat06061999@gmail.com>
2022-01-31 20:19:55 -0800

    add some stuff

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello world!")
+import discord
+import os
+from discord.ext import commands
+
+
+TOKEN = os.environ["TOKEN"]
+
+
+bot = commands.Bot(command_prefix="b!")
+
+
+@bot.event
+async def on_ready():
+    print('BlazingFart is ready, version ' + str(discord.__version__))
+
+
+bot.run(TOKEN, bot=True, reconnect=True)
