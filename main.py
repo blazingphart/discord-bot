31a556f16fc90a09a9935b96a04cb1187e8fd88d
phatmai99 <maihungphat06061999@gmail.com>
2022-02-02 02:00:14 -0800

    stuff

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+from lib2to3.pgen2 import token
 import discord
 import os
 from discord.ext import commands
+import pymongo
+from dotenv import load_dotenv
+
+from database import MONGO_CLIENT
 
 
-TOKEN = os.environ["TOKEN"]
-
+load_dotenv()
+TOKEN = os.getenv("TOKEN")
+MONGO_CLIENT = os.getenv("MONGO_CLIENT")
 
 bot = commands.Bot(command_prefix="b!")
 
